71424dabb47d90c900138bc721b4c53699046f0c
shreshtha-132 <shreshthakumargupta@gmail.com>
2023-07-06 11:54:19 +0530

    completed in one go
    -> simple stateful widget app using math library to generate a random number and then updating the image on click so as to help in decision making.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  print('hello');
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Ask Me Anything')),
+          backgroundColor: Colors.blue,
+        ),
+        body: rollBall(),
+      ),
+    ),
+  );
+}
+
+class rollBall extends StatefulWidget {
+  // const rollBall({Key? key}) : super(key: key);
+
+  @override
+  _rollBallState createState() => _rollBallState();
+}
+
+class _rollBallState extends State<rollBall> {
+  int number = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              number = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$number.png'),
+        ),
+      ),
+    );
+  }
 }
